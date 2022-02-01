b9cf1a36675432453f475e82a7be6ac134b8faec
root <denis.genitoni@teck-developer.com>
2022-02-01 15:51:54 +0100

    caricamento modifiche installazione

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,4 @@ Route::get('/', function () {
 });
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::get('/customers/info', [CustomersController::class, 'index'])->name('informations');
