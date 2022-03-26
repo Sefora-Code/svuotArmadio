10b1b442ecd9a6c30feadb878fbcd6303ea2887f
Ale <alessandro.gallo.a@gmail.com>
2022-03-26 13:32:31 +0100

    Improved pick-up list and details page
    UI improvements.
    Still missing js ajax calls to server to edit the order details.

--- resources/views/pickups/home.blade.php
+++ resources/views/pickups/home.blade.php
@@ -2,28 +2,39 @@
 
 @section('title'){{ __('front.title_home') }}@stop
 
+@section('custom_css')
+<style>
+    td{vertical-align: middle !important;}
+    .btn-info{color: #fff;}
+    .btn-success{background-color: #5cb85c; border-color: #4cae4c; }
+</style>
+@endsection
+
 @section('body')
-		<div class="row storico-ritiri">
-			<div class="row title mb-5"><h1>Lista ritiri</h1></div>
+		<div class="row lista-ritiri">
+			<div class="row title mb-5 text-center"><h1>Lista ritiri di oggi</h1></div>
 			<table class="table">
 				<thead>
 					<tr>
-						<th scope="col">Ritiro</th>
-						<th scope="col">Data Prenotazione</th>
-						<th scope="col">Data ritiro</th>
-						<th scope="col">Indirizzo Ritiro</th>
-						<th scope="col">Stato</th>
+<!-- 						<th scope="col">Ritiro</th> -->
+<!-- 						<th scope="col">Data Prenotazione</th> -->
+<!-- 						<th scope="col">Data ritiro</th> -->
+						<th scope="col">Fascia oraria</th>
+						<th scope="col" class="text-center">Indirizzo Ritiro</th>
+<!-- 						<th scope="col">Stato</th> -->
+						<th scope="col"></th>
 					</tr>
 				</thead>
 				<tbody>
 				@foreach($orders as $order)
 					@if($order->orderDetails->pickup_date == date('Y-m-d 00:00:00'))
     					<tr>
-    						<th scope="row"><a href="{{route('pickups-detail-emp', $order->id)}}">{{$order->id}}</a></th>
-    						<td>{{ date("d / m / Y", strtotime($order->created_at)) }}</td>
-    						<td>{{ date("d / m / Y", strtotime($order->orderDetails->pickup_date)) }}</td>
+<!--     						<th scope="row"><a href="{{route('pickups-detail-emp', $order->id)}}">{{$order->id}}</a></th> -->
+<!--     						<td>{{ date("d / m / Y", strtotime($order->created_at)) }}</td> -->
+<!--     						<td>{{ date("d / m / Y", strtotime($order->orderDetails->pickup_date)) }}</td> -->
+    						<td>{{ $order->orderDetails->time_frame }}</td>
     						<td>{{ $order->orderDetails->shipping_address }}</td>
-    						<td>
+<!--     						<td>
     						@switch($order->fullfilled)
     							@case(0)
     								Registrato
@@ -44,6 +55,13 @@
     								Non si riesce a completare
     								@break
 							@endswitch
+     						</td> -->
+    						<td>
+    							<a class="btn btn-info mb-1" href="{{route('pickups-detail-emp', $order->id)}}">Dettagli</a>
+    							<br>
+    							<button class="btn btn-success mb-1" onclick="editOrder({{$order->id}}, 1)">Ritirato</button>
+    							<br>
+    							<button class="btn btn-danger" onclick="editOrder({{$order->id}}, 2)">NON Ritirato</button>
     						</td>
     					</tr>
 					@endif
@@ -52,4 +70,18 @@
 			</table>
 		</div>
 		
-@endsection		
+@endsection
+
+@section('custom_js')
+<script>
+function editOrder(orderId, newState)
+{
+	if (confirm("Sei sicuro?"))
+	{
+		// chiamata ajax per modificare l'ordine
+	}
+	else
+		alert("Azione annullata");
+}
+</script>
+@endsection
--- resources/views/pickups/pickup-details.blade.php
+++ resources/views/pickups/pickup-details.blade.php
@@ -2,46 +2,81 @@
 
 @section('title'){{ __('front.title_home') }}@stop
 
+@section('custom_css')
+<style>
+    .btn-info{color: #fff;}
+    .btn-success{background-color: #5cb85c; border-color: #4cae4c; }
+</style>
+@endsection
+
 @section('body')
 
 		<div class="row dettagli-ritiro">
-			<div class="row title mb-5"><h1>Dettaglio ritiro {{$order->id}}</h1></div>
+			<div class="row title mb-5"><h1>Dettaglio ritiro</h1></div>
 			<div class="row">
 				<div class="col-lg-12">
 					<div class="row mb-2">
-						<div class="col text-right">Indirizzo specificato:</div>
+						<div class="col text-right"><b>Indirizzo:</b></div>
 						<div class="col">{{ $order->orderDetails->shipping_address }}</div>
-					</div>
-					<div class="row mb-2">
-						<div class="col text-right">Data di prenotazione:</div>
-						<div class="col">{{ date("d / m / Y", strtotime($order->created_at)) }}</div>
-					</div>
-					<div class="row mb-2">
-						<div class="col text-right">Data del ritiro:</div>
-						<div class="col">{{ date("d / m / Y", strtotime($order->orderDetails->pickup_date)) }} - {{ $order->orderDetails->time_frame }}</div>
 					</div>
 					<!--div class="row">
 						<div class="col">Peso:</div>
 						<div class="col">1-5Kg</div>
 					</div -->
 					<div class="row mb-2">
-						<div class="col text-right">Volume:</div>
+						<div class="col text-right"><b>Volume:</b></div>
 						<div class="col">1-{{$order->orderDetails->volume}} Sacchetti</div>
 					</div>
 					<div class="row mb-2">
-						<div class="col text-right">Nome sul campanello e note:</div>
+						<div class="col text-right"><b>Nome sul campanello e note:</b></div>
 						<div class="col">{{$order->orderDetails->notes}}</div>
 					</div>
-					<div class="row mb-2">
+					<div class="row mb-2 mt-4">
 						<div class="col text-right">Costo:</div>
 						<div class="col">-</div>
 					</div>
-					<div class="row">
+					<div class="row mb-2">
 						<div class="col text-right">Stato:</div>
 						<div class="col">{{ $order->fulfilled ? "Completato" : "Accettato" }}</div>
 					</div>
+					<div class="row mb-2">
+						<div class="col text-right">Data di prenotazione:</div>
+						<div class="col">{{ date("d / m / Y", strtotime($order->created_at)) }}</div>
+					</div>
+					<div class="row mb-5">
+						<div class="col text-right">Data del ritiro:</div>
+						<div class="col">{{ date("d / m / Y", strtotime($order->orderDetails->pickup_date)) }} - {{ $order->orderDetails->time_frame }}</div>
+					</div>
+					
 				</div>
 			</div>
 		</div>
+		<div class="row mb-4">
+			<div class="col-6 text-right">
+				<button class="btn btn-success mb-1" onclick="editOrder({{$order->id}}, 1)">RITIRATO</button>
+			</div>
+			<div class="col-6">
+				<button class="btn btn-danger" onclick="editOrder({{$order->id}}, 2)">NON RITIRATO</button>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-12 text-center">
+				<button class="btn btn-info" onclick="window.history.back();">INDIETRO</button>
+			</div>
+		</div>
 		
-@endsection		
+@endsection	
+	
+@section('custom_js')
+<script>
+function editOrder(orderId, newState)
+{
+	if (confirm("Sei sicuro?"))
+	{
+		// chiamata ajax per modificare l'ordine
+	}
+	else
+		alert("Azione annullata");
+}
+</script>
+@endsection
